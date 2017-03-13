d61e0a061493f6d84b2ab9c9a050c3212a2074d6
root <root@vps386828.ovh.net>
2017-03-13 22:15:09 +0100

    hho git on push switch handling to php instead git and shell exec

--- gitevent/push.php
+++ gitevent/push.php
@@ -1,3 +1,6 @@
 <?php
-system("git log  --oneline -10 --author=\"Exibi\" > /var/www/board/gentelella/git.log");
+shell_exec("cd /var/www/board/gentelella && git log --oneline -10 --author=Exibi > /var/www/board/gentelella/git.log");
+
+echo "saved";
+var_dump(json_decode(file_get_contents("php://input"),true));
 ?>
