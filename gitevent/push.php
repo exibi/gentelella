280ab36b4631ab7f57275e7511f773206604e7bb
root <root@vps386828.ovh.net>
2017-03-13 22:19:28 +0100

    fix to last commit

--- gitevent/push.php
+++ gitevent/push.php
@@ -1,6 +1,3 @@
 <?php
-shell_exec("cd /var/www/board/gentelella && git log --oneline -10 --author=Exibi > /var/www/board/gentelella/git.log");
-
-echo "saved";
 var_dump(json_decode(file_get_contents("php://input"),true));
 ?>
